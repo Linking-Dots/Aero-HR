f059ecdcfddc921edecc202ed106c5bacbe54485
Emam Hosen <emamhsajeeb@gmail.com>
2024-08-12 21:19:07 +0600

    ok

--- public/build/assets/Edit-DP1LcCAN.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-CYJc101Q.js";import{n as e}from"./App-BAtq0dKA.js";const a=e(t.jsx("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"}),"Delete"),c=e(t.jsx("path",{d:"M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75z"}),"Edit");export{a as D,c as E};
