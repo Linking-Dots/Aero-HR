5d9f11cc3d68b4568e864008c6452559b479089d
Emam Hosen <emamhsajeeb@gmail.com>
2024-09-11 00:46:13 +0600

    ok

---
+++ public/build/assets/UpdatePasswordForm-VBCbFlDv.js
@@ -0,0 +1,1 @@
+import{W as a,a as e}from"./app-DunBBaRZ.js";import{r}from"./vendor-BBvAslwD.js";const o=()=>(r.useState(!1),a({email:"",password:"",remember:!1}),e("div",{className:"auth-page-wrapper pt-5"}));export{o as default};
