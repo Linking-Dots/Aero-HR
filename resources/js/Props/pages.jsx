662e115471d09b272064dc4408aa99df55a59846
Emam Hosen <emamhsajeeb@gmail.com>
2024-09-29 17:17:15 +0600

    ok

--- resources/js/Props/pages.jsx
+++ resources/js/Props/pages.jsx
@@ -15,31 +15,31 @@ import GroupIcon from '@mui/icons-material/Group';
 import { AccountCircle, ExitToApp, Settings } from '@mui/icons-material';
 // Function to create pages array
 export const getPages = (permissions) => [
-    { name: 'Dashboard', icon: <DashboardIcon sx={{ ml: 2 }}/>, route: 'dashboard' },
-    { name: 'Leaves', icon: <LogoutIcon sx={{ ml: 2 }}/>, route: 'leaves-employee' },
-    { name: 'Attendances', icon: <CalendarTodayIcon sx={{ ml: 2 }}/>, route: 'attendance-employee' },
+    { name: 'Dashboard', icon: <DashboardIcon/>, route: 'dashboard' },
+    { name: 'Leaves', icon: <LogoutIcon/>, route: 'leaves-employee' },
+    { name: 'Attendances', icon: <CalendarTodayIcon/>, route: 'attendance-employee' },
     ...(permissions.includes('read employee') ? [
         {
-            name: 'Employees', icon: <PeopleIcon sx={{ ml: 2 }}/>, subMenu: [
-                ...(permissions.includes('read employee') ? [{ name: 'All Employees', icon: <PeopleIcon />, route: 'employees' }] : []),
-                ...(permissions.includes('read holidays') ? [{ name: 'Holidays', icon: <EventNoteIcon />, route: 'holidays' }] : []),
-                ...(permissions.includes('read leaves') ? [{ name: 'Leaves (Admin)', icon: <LogoutIcon />, route: 'leaves', badge: { content: '1', className: 'badge rounded-pill bg-primary float-end' } }] : []),
-                ...(permissions.includes('read leaves') ? [{ name: 'Leave Settings', icon: <SettingsIcon />, route: 'leave-settings' }] : []),
-                ...(permissions.includes('read attendances') ? [{ name: 'Attendances (Admin)', icon: <CalendarTodayIcon />, route: 'attendances' }] : []),
-                ...(permissions.includes('read departments') ? [{ name: 'Departments', icon: <HomeIcon />, route: 'departments' }] : []),
-                ...(permissions.includes('read designations') ? [{ name: 'Designations', icon: <WorkIcon />, route: 'designations' }] : []),
-                ...(permissions.includes('read timesheet') ? [{ name: 'Timesheet', icon: <ListAltIcon />, route: 'timesheet' }] : []),
+            name: 'Employees', icon: <PeopleIcon/>, subMenu: [
+                ...(permissions.includes('read employee') ? [{ name: 'All Employees', icon: <PeopleIcon sx={{ ml: 2 }}/>, route: 'employees' }] : []),
+                ...(permissions.includes('read holidays') ? [{ name: 'Holidays', icon: <EventNoteIcon sx={{ ml: 2 }}/>, route: 'holidays' }] : []),
+                ...(permissions.includes('read leaves') ? [{ name: 'Leaves (Admin)', icon: <LogoutIcon sx={{ ml: 2 }}/>, route: 'leaves', badge: { content: '1', className: 'badge rounded-pill bg-primary float-end' } }] : []),
+                ...(permissions.includes('read leaves') ? [{ name: 'Leave Settings', icon: <SettingsIcon sx={{ ml: 2 }}/>, route: 'leave-settings' }] : []),
+                ...(permissions.includes('read attendances') ? [{ name: 'Attendances (Admin)', icon: <CalendarTodayIcon sx={{ ml: 2 }}/>, route: 'attendances' }] : []),
+                ...(permissions.includes('read departments') ? [{ name: 'Departments', icon: <HomeIcon sx={{ ml: 2 }}/>, route: 'departments' }] : []),
+                ...(permissions.includes('read designations') ? [{ name: 'Designations', icon: <WorkIcon sx={{ ml: 2 }}/>, route: 'designations' }] : []),
+                ...(permissions.includes('read timesheet') ? [{ name: 'Timesheet', icon: <ListAltIcon sx={{ ml: 2 }}/>, route: 'timesheet' }] : []),
             ]
         }
     ] : []),
     {
-        name: 'Projects', icon: <WorkIcon sx={{ ml: 2 }}/>, subMenu: [
+        name: 'Projects', icon: <WorkIcon/>, subMenu: [
             // { name: 'Projects', icon: <WorkIcon />, route: 'dashboard' },
-            { name: 'Daily Works', icon: <ListAltIcon />, route: 'dailyWorks' },
-            { name: 'Daily Work Summary', icon: <ListIcon />, route: 'dailyWorkSummary' }
+            { name: 'Daily Works', icon: <ListAltIcon sx={{ ml: 2 }}/>, route: 'dailyWorks' },
+            { name: 'Daily Work Summary', icon: <ListIcon sx={{ ml: 2 }}/>, route: 'dailyWorkSummary' }
         ]
     },
-    ...(permissions.includes('read users') ? [{name: 'Users', icon: <GroupIcon sx={{ ml: 2 }}/>, route: 'users'}] : []),
-    ...(permissions.includes('read settings') ? [{name: 'Settings', icon: <Settings sx={{ ml: 2 }}/>, route: 'company-settings'}] : []),
+    ...(permissions.includes('read users') ? [{name: 'Users', icon: <GroupIcon/>, route: 'users'}] : []),
+    ...(permissions.includes('read settings') ? [{name: 'Settings', icon: <Settings/>, route: 'company-settings'}] : []),
 
 ];
