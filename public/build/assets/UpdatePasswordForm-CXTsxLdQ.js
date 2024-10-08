a616148fbf7e5e26078744b89fe4d12cd7a45ee4
Emam Hosen <emamhsajeeb@gmail.com>
2024-10-09 00:07:44 +0600

    ok

--- public/build/assets/UpdatePasswordForm-CXTsxLdQ.js
+++
@@ -1,1 +0,0 @@
-import{W as a,a as e}from"./app-CUflWZJ2.js";import{r}from"./vendor-B6hDaovV.js";const o=()=>(r.useState(!1),a({email:"",password:"",remember:!1}),e("div",{className:"auth-page-wrapper pt-5"}));export{o as default};
