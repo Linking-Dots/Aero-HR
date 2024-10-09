9da9b052c310d3a959195f54b5bf907264c5dbfb
Emam Hosen <emamhsajeeb@gmail.com>
2024-10-09 11:52:52 +0600

    ok

--- public/build/assets/Upload-BGLGrciy.js
+++
@@ -1,1 +0,0 @@
-import{v as o,w as a}from"./vendor-B6hDaovV.js";const t=o(a.jsx("path",{d:"M5 20h14v-2H5zm0-10h4v6h6v-6h4l-7-7z"}),"Upload");export{t as U};
---
+++ public/build/assets/Upload-CyZ5FvQY.js
@@ -0,0 +1,1 @@
+import{v as o,w as a}from"./vendor-EgrJM_gj.js";const t=o(a.jsx("path",{d:"M5 20h14v-2H5zm0-10h4v6h6v-6h4l-7-7z"}),"Upload");export{t as U};
