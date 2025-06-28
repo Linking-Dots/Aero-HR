9c0de055c8288ed2dc5c4b3bdbe4697f4923d09d
Emam Hosen <emamhsajeeb@gmail.com>
2025-06-28 18:37:19 +0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   resources/views/app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
 
     <!-- Security & Performance -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
